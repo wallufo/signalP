bcbddcfdf723caa9005224a2b3ba996a9d790448
wallufo <877546504@qq.com>
2020-05-11 19:00:41 +0800

    2020.5.11（2）

--- SSB-BC.py
+++ SSB-BC.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-t=np.linspace(-10.0,10.0,0,1000)
-def sinSignal(A,w,t,phi):
-    return A*np.cos(w*t+phi)
+fl=[697,770,852,941] #低频频率
+fh=[1209,1336,1477] #高频频率
+a={'1':[fl[0],fh[0]]}
+print(a['1'])
--- wavetest.py
+++ wavetest.py
@@ -9,14 +9,14 @@ def goertzel(simple,freq_indices,N):
     w=[]
     c=[]
     p=[]
-    for i in range(0,7):
+    for i in range(0,8):
         w.append(2*np.pi*freq_indices[i]/N)
         wc=w[i]
         ans=2*np.cos(wc)
         c.append(ans)
     q1=q2=0
-    for i in range(0,7):
-        for j in range(0,N-1):
+    for i in range(0,8):
+        for j in range(0,N):
             q0=c[i]*q1-q2+simple[j]
             q2=q1
             q1=q0
@@ -32,8 +32,8 @@ def findIndex(simple):
     return index
 path='e:\\signalP\\'
 filepath=os.listdir(path)
-print(filepath[10])
-f=we.open(path+filepath[10],'rb')
+print(filepath[9])
+f=we.open(path+filepath[9],'rb')
 params = f.getparams()
 nchannels, sampwidth, framerate, nframes = params[:4]
 strData = f.readframes(nframes)#读取音频，字符串格式
@@ -41,11 +41,11 @@ waveData = np.fromstring(strData,dtype=np.int16)#将字符串转化为int
 waveData = waveData*1.0/(max(abs(waveData)))#wave幅值归一化
 f.close()
 time = np.arange(0,nframes)*(1.0 / framerate)
-plt.plot(time,waveData)
-plt.xlabel("Time(s)")
-plt.ylabel("Amplitude")
-plt.title("Single channel wavedata")
-plt.show()
+# plt.plot(time,waveData)
+# plt.xlabel("Time(s)")
+# plt.ylabel("Amplitude")
+# plt.title("Single channel wavedata")
+# plt.show()
 f=[697,770,852,941,1209,1336,1477,1633]
 N=8000
 i=0
@@ -57,12 +57,11 @@ num=[]
 freq_indices=[]
 tm=np.array([[1,2,3,65],[4,5,6,66],[7,8,9,67],[42,0,35,68]])
 limit=0.5*max(waveData)
-print(limit)
 datalength=len(time)
 for fi in f:
     freq_indices.append(int(fi/fs*N))
-for i in range(0,datalength-1):
-    if j>=10:
+for i in range(0,datalength):
+    if j>=11:
         break
     if np.abs(waveData[k])>limit :
         x1=waveData[k:k+N]
@@ -74,9 +73,4 @@ for i in range(0,datalength-1):
         j=j+1
     else:
         k=k+1
-print(num)
-# x1=waveData[6*N:7*N]
-# xk=goertzel(x1,freq_indices,N)
-# num.append(findIndex(xk[0:3]))
-# num.append(findIndex(xk[4:7]))
-# print(tm[num[0],num[1]])
+
